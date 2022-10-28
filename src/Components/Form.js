7962d9c8c242f8ba6736dfddcb17e58322c235d9
Reddy-react <sudharsanreddy101@gmail.com>
2022-10-28 11:42:32 +0530

    Form

---
+++ src/Components/Form.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Form(props) {
+  return (
+    <div>
+        <h1>Form page </h1>
+    </div>
+  )
+}
+
+Form.propTypes = {}
+
+export default Form
